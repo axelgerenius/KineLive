56200307ed0cc6161d7187935b8449ca9c5c3b6d
jsebfranck <jsebfranck@gmail.com>
2016-03-19 15:32:22 +0100

    fiche suivi

--- kinelive/www/page.ficheSuivi/controller.js
+++ kinelive/www/page.ficheSuivi/controller.js
@@ -1,7 +1,13 @@
 
-angular.module('page.ficheSuivi').controller('ficheSuiviController', ['$scope', '$location',
-    function($scope, $location) {
+angular.module('page.ficheSuivi').controller('ficheSuiviController', ['$scope', '$location', function($scope, $location) {
+
+    $scope.criterias = [{type:'Douleur', level: '2'}, {type:'Mobilité', level: '3'}, {type:'Amplitude', level: '6'}];
+
+    $scope.addCriteria = function() {
+        $scope.criterias.push({});
+    };
 
     $scope.doFicheSuiviCreation = function() {
+
     };
 }]);
