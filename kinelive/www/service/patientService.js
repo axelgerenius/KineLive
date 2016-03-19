249ee8b141c6cf54182f72631143c2769c29195d
jsebfranck <jsebfranck@gmail.com>
2016-03-19 14:09:42 +0100

    Clients listing

--- kinelive/www/service/patientService.js
+++ kinelive/www/service/patientService.js
@@ -1,27 +1,33 @@
-angular.module('service.patientService', [])
-
-.factory('patientService', [function() {
+angular.module('service.patientService', []).factory('patientService', [function() {
 	return {
 		patientList : [
 			{
 				"id" : 1,
 				"firstname" : "Mike",
-				"lastname" : "Portnoy"
+				"lastname" : "Portnoy",
+				"email" : "mikep@yahoo.fr",
+				"photo" : "http://bootdey.com/img/Content/user_1.jpg"
 			},
 			{
 				"id" : 2,
 				"firstname" : "Neal",
-				"lastname" : "Morse"
+				"lastname" : "Morse",
+				"email": "morse.neal@gmail.om",
+				"photo" : "http://bootdey.com/img/Content/user_2.jpg"
 			},
 			{
 				"id" : 3,
 				"firstname" : "Roine",
-				"lastname" : "Stolt"
+				"lastname" : "Stolt",
+				"email": "roine.stolt@gmail.com",
+				"photo" : "http://bootdey.com/img/Content/user_3.jpg"
 			},
 			{
 				"id" : 4,
 				"firstname" : "Pete",
-				"lastname" : "Trewavas"
+				"lastname" : "Trewavas",
+				"email": "pete@trewavas.com",
+				"photo" : "http://bootdey.com/img/Content/user_1.jpg"
 			}
 		],
 
