f824cd5d7671a164198b9eaea58ac0941b1b11dd
simvetanylen <oreste.viron@gmail.com>
2016-03-19 01:35:51 +0100

    Suppression ancienne appli

--- modules/patients/client/patients.client.module.js
+++
@@ -1,7 +0,0 @@
-(function (app) {
-  'use strict';
-
-  app.registerModule('patients', ['core']);// The core module is required for special route handling; see /core/client/config/core.client.routes
-  app.registerModule('patients.services');
-  app.registerModule('patients.routes', ['ui.router', 'patients.services']);
-}(ApplicationConfiguration));
