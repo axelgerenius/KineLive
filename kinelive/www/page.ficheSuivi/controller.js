361fc0f57cd9205327823046908e304ee809cf5e
jsebfranck <jsebfranck@gmail.com>
2016-03-19 17:12:56 +0100

    Enregistrement son

--- kinelive/www/page.ficheSuivi/controller.js
+++ kinelive/www/page.ficheSuivi/controller.js
@@ -10,7 +10,7 @@ angular.module('page.ficheSuivi').controller('ficheSuiviController', ['$scope', 
     $scope.doFicheSuiviCreation = function() {
 
     };
-        
+
     //Minimal slider config
     $scope.minSlider = {
         value: 10
@@ -23,9 +23,37 @@ angular.module('page.ficheSuivi').controller('ficheSuiviController', ['$scope', 
             ceil: 10,
             floor: 0,
             showTicksValues: true
-            
+
         }
     };
+
+    var captureError = function(e) {
+        console.log('captureError' ,e);
+    };
+
+    var captureSuccess = function(e) {
+        console.log('captureSuccess');
+        console.dir(e);
+        $scope.sound.file = e[0].localURL;
+        $scope.sound.filePath = e[0].fullPath;
+    };
+
+    $scope.record = function() {
+        navigator.device.capture.captureAudio(
+            captureSuccess,captureError,{duration:10});
+    };
+
+    $scope.play = function() {
+        if(!$scope.sound.file) {
+            navigator.notification.alert("Record a sound first.", null, "Error");
+            return;
+        }
+        var media = new Media($scope.sound.file, function(e) {
+            media.release();
+        }, function(err) {
+            console.log("media err", err);
+        });
+        media.play();
+    };
 }]);
-    
     
