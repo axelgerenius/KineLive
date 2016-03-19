c67011666203d4eaf4bba8e98ecf7c68e6a31b1e
simvetanylen <oreste.viron@gmail.com>
2016-03-19 13:53:31 +0100

    navigation entre onglets

--- kinelive/www/ui.mainComponent.tabBarPatient/controller.js
+++ kinelive/www/ui.mainComponent.tabBarPatient/controller.js
@@ -1,6 +1,15 @@
 
 angular.module('ui.mainComponent.tabBarPatient')
 
-.controller('uiMainComponentTabBarPatientController', ['$scope', '$location', 
-		function($scope, $location) {
+.controller('uiMainComponentTabBarPatientController', ['$scope', '$location', '$state',
+		function($scope, $location, $state) {
+
+	$scope.goToProfile = function() {
+
+		$state.go("profile", {"id" : $state.params.id});
+	};
+
+	$scope.goToReeducation = function() {
+		$state.go("reeducation", {"id" : $state.params.id});
+	};
 }]);
